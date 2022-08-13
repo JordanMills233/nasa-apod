b9ae18ae46bb0b11cd9f890d9b42e53cd603de02
JordanMills233 <jordanmills233@gmail.com>
2022-08-13 14:33:08 +0100

    Styling 2.1

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,47 +6,50 @@ import "bootstrap/dist/css/bootstrap.min.css";
 //mport { borderRadius } from "polished";
 
 export const linkStyle = {
-  fontSize: "16px",
-  fontWeight: "500",
-  padding: "5px 10px 5px 10px",
-  borderRadius: "5px",
-  background: "black",
-  textDecoration: "none",
-  color: "white",
+    fontSize: "16px",
+    fontWeight: "500",
+    padding: "5px 10px 5px 10px",
+    borderRadius: "5px",
+    background: "black",
+    textDecoration: "none",
+    color: "white",
 };
 
 export default function Home() {
-  const [chosenDate, setChosenDate] = useState("");
+    const [chosenDate, setChosenDate] = useState("");
 
-  const handleSubmit = (event) => {
-    console.log("handleSubmit ran");
-    event.preventDefault();
-  };
+    const handleSubmit = (event) => {
+        console.log("handleSubmit ran");
+        event.preventDefault();
+    };
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <div className="Submit-container">
-          <form onSubmit={handleSubmit}>
-            <input
-              type="text"
-              name="name"
-              placeholder="YYYY-MM-DD"
-              value={chosenDate}
-              onChange={(event) => setChosenDate(event.target.value)}
-            />
-          </form>
-          <div className="Submit-button">
-            <Link
-              to={`/nasa-photo`}
-              state={{ date: chosenDate }}
-              style={linkStyle}
-            >
-              See into the stars
-            </Link>
-          </div>
+    return (
+        <div className="App">
+            <header className="App-header">
+                <div className="Submit-container">
+                    <form onSubmit={handleSubmit}>
+                        <input
+                            className="pls"
+                            type="date"
+                            name="name"
+                            placeholder="YYYY-MM-DD"
+                            value={chosenDate}
+                            onChange={(event) =>
+                                setChosenDate(event.target.value)
+                            }
+                        />
+                    </form>
+                    <div className="Submit-button">
+                        <Link
+                            to={`/nasa-photo`}
+                            state={{ date: chosenDate }}
+                            style={linkStyle}
+                        >
+                            See into the stars
+                        </Link>
+                    </div>
+                </div>
+            </header>
         </div>
-      </header>
-    </div>
-  );
+    );
 }
